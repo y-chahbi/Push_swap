bf7a2e0685ad15f6f60270d4e09885a3ee73de3e
Youssef Chahbi <ychahbi@e2r10p13.1337.ma>
2023-02-18 19:00:04 +0100

    sjh

--- extra/last_sort.c
+++ extra/last_sort.c
@@ -6,22 +6,12 @@
 /*   By: ychahbi <ychahbi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 11:35:25 by ychahbi           #+#    #+#             */
-/*   Updated: 2023/02/14 22:08:03 by ychahbi          ###   ########.fr       */
+/*   Updated: 2023/02/18 18:59:29 by ychahbi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-//void	last_sort(t_push_data *t_data)
-//{
-//	while (is_sorted_a(t_data) == 0)
-//	{
-//		if (find_smal_dex(t_data) < t_data->size_a / 2)
-//			rotate_a(t_data);
-//		else
-//			rev_rot_a(t_data);
-//	}
-//}
 void	last_sort(t_push_data *t_data)
 {
 	int	min_idx;
